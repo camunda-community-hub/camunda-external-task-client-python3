0d654eddf56c86f8d84b748e426da500e0041bdd
Yogesh Naik <yogeshnaik@Yogeshs-MacBook-Pro.local>
2020-05-16 21:20:36 +0530

    Added logging with context in tasks example + asyncAfter in BPMN

--- examples/tasks_example.py
+++ examples/tasks_example.py
@@ -3,9 +3,11 @@ import logging
 from datetime import datetime
 
 import time
+from random import randint
 
 from camunda.external_task.external_task import ExternalTask
 from camunda.external_task.external_task_worker import ExternalTaskWorker
+from camunda.utils.log_utils import log_with_context
 
 logger = logging.getLogger(__name__)
 
@@ -51,19 +53,20 @@ def configure_logging():
 
 
 async def handle_task(task: ExternalTask):
-    logging.info(f"handleTask() - {task}")
+    log_context = {"WORKER_ID": task.get_worker_id(), "TASK_ID": task.get_task_id(), "TOPIC": task.get_topic_name()}
+    log_with_context("handle_task started", log_context)
 
     # simulate task execution
-    logger.info(f"handleTask() - business logic execution started for task: {task.get_task_id()}")
-    await asyncio.sleep(10)
+    log_with_context(f"handle_task - business logic execution started for task", log_context)
+    await asyncio.sleep(randint(0, 10))
 
     failure = random_true()
     bpmn_error = False if failure else random_true()
 
     # override the values to simulate success/failure/BPMN error explicitly
     failure, bpmn_error = False, False
-    logger.info(f"handleTask() - business logic executed for topic: {task.get_topic_name()}, "
-                f"task: {task.get_task_id()}, failure: {failure}, bpmn_error: {bpmn_error}")
+    log_with_context(f"handle_task - business logic executed: failure: {failure}, bpmn_error: {bpmn_error}",
+                     log_context)
 
     if failure:
         return task.failure("task failed", "failed task details", 3, 5000)
