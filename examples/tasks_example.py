a962471446479c818ab71a6a062cb031c5fba661
Yogesh Naik <yogeshnaik@Yogeshs-MacBook-Pro.local>
2020-05-16 21:56:14 +0530

    logged random execution time of task in example + minor refactoring

--- examples/tasks_example.py
+++ examples/tasks_example.py
@@ -1,9 +1,9 @@
 import asyncio
 import logging
 from datetime import datetime
+from random import randint
 
 import time
-from random import randint
 
 from camunda.external_task.external_task import ExternalTask
 from camunda.external_task.external_task_worker import ExternalTaskWorker
@@ -57,22 +57,27 @@ async def handle_task(task: ExternalTask):
     log_with_context("handle_task started", log_context)
 
     # simulate task execution
-    log_with_context(f"handle_task - business logic execution started for task", log_context)
-    await asyncio.sleep(randint(0, 10))
+    execution_time = randint(0, 10)
+    log_with_context(f"handle_task - business logic execution started for task: "
+                     f"it will execute for {execution_time} seconds", log_context)
+    await asyncio.sleep(execution_time)
 
+    # simulate that task results randomly into failure/BPMN error/complete
     failure = random_true()
     bpmn_error = False if failure else random_true()
-
-    # override the values to simulate success/failure/BPMN error explicitly
+    # override the values to simulate success/failure/BPMN error explicitly (if needed)
     failure, bpmn_error = False, False
     log_with_context(f"handle_task - business logic executed: failure: {failure}, bpmn_error: {bpmn_error}",
                      log_context)
 
+    return await __handle_task_result(task, failure, bpmn_error)
+
+
+async def __handle_task_result(task, failure, bpmn_error):
     if failure:
         return task.failure("task failed", "failed task details", 3, 5000)
     elif bpmn_error:
         return task.bpmn_error("BPMN_ERROR_CODE")
-
     return task.complete({"success": True, "task_completed_on": str(datetime.now())})
 
 
