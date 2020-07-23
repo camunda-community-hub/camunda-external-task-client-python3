4207ce5d7dbab5ca1e1be487c3f2fac419b8665f
Michael Lawrenson <michael.lawrenson@cubrc.org>
2020-07-23 12:49:22 -0400

    Include requirements.txt dependencies when installing

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     description='Camunda External Task Client for Python 3',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
+    install_requires=open('requirements.txt').read().splitlines(),
     include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
