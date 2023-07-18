e36979e853d9e02e7fbcc489acc01e12c8d35982
Daniel Bailo <danbailoufms@gmail.com>
2023-07-18 16:59:59 -0300

    feat: improved authbearer serializer

--- camunda/utils/auth_bearer.py
+++ camunda/utils/auth_bearer.py
@@ -1,10 +1,25 @@
+from typing import Any
+
 from pydantic import BaseModel, validator
 
 
 class AuthBearer(BaseModel):
     access_token: str
 
+    @validator('access_token', pre=True)
+    @classmethod
+    def get_token_from_dict(cls, value: str | dict[str, Any]) -> str:
+        if isinstance(value, str):
+            return value
+        if not isinstance(value, dict):
+            raise ValueError('token should be dict or str')
+        return value.get('access_token')
+
     @validator('access_token')
     @classmethod
     def concat_bearer(cls, value: str) -> str:
-        return f'Bearer {value}'
+        if not any([
+            value.startswith('Bearer'),
+            value.startswith('bearer')
+        ]):
+            return f'Bearer {value}'
