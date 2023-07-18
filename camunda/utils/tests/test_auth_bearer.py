6b817c1062ac625328d22976a9833031a995e11b
Daniel Bailo <danbailoufms@gmail.com>
2023-07-18 17:45:14 -0300

    tests: testing when access token is dict

--- camunda/utils/tests/test_auth_bearer.py
+++ camunda/utils/tests/test_auth_bearer.py
@@ -8,8 +8,20 @@ class TestAuthBasic(TestCase):
 
     reffer - https://pyjwt.readthedocs.io/en/stable/
     """
-    def test_concat_bearer(self):
+    def test_str_token_bearer(self):
         token = ('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkZW1vIn0'
                  '.NbMsjy8QQ5nrjGTXqdTrJ6g0dqawRvZAqp4XvNt437M')
         auth_bearer = AuthBearer(access_token=token)
         self.assertEqual(auth_bearer.access_token, f'Bearer {token}')
+
+    def test_dict_token_bearer(self):
+        token = ('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkZW1vIn0'
+                 '.NbMsjy8QQ5nrjGTXqdTrJ6g0dqawRvZAqp4XvNt437M')
+        auth_bearer = AuthBearer(access_token={'access_token': token})
+        self.assertEqual(auth_bearer.access_token, f'Bearer {token}')
+
+    def test_error_dict_token_bearer(self):
+        token = ('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkZW1vIn0'
+                 '.NbMsjy8QQ5nrjGTXqdTrJ6g0dqawRvZAqp4XvNt437M')
+        with self.assertRaises(KeyError):
+            AuthBearer(access_token={'token': token})
